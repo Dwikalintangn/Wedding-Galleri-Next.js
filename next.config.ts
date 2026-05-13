4c06b069d2414b8f36f33c4acc67dba5803b5d3d
Dwika Lintang Nugraha <dwikalintangn@gmail.com>
2026-05-13 12:04:57 +0700

    setup github actions deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // 🔥 wajib untuk static deploy
+
   images: {
+    unoptimized: true, // 🔥 wajib untuk GitHub Pages
     remotePatterns: [
       {
         protocol: "https",
